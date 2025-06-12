941fb9a9f445e4c81cc592267b3d605b4d8f0077
kmnnmew <shimhayun08@gmail.com>
2025-06-12 14:11:18 +0900

    수정

--- SP/main.js
+++ SP/main.js
@@ -135,4 +135,7 @@ var app = http.createServer(function(request,response){
      response.end('Not found');
   }
 });
-app.listen(3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
